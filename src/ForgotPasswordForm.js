a6a1b9ac911b4e52b42086c5e0890d27d8d8b804
gavin <602315302@qq.com>
2017-08-18 14:20:29 +0800

    todo

--- src/ForgotPasswordForm.js
+++ src/ForgotPasswordForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Retreat from './img/retreat.svg'
 import ForgotPasswordFormStyle from './ForgotPasswordForm.css'
+import $ from 'jquery'
 
 export default class ForgotPasswordForm extends Component {
     render(){
@@ -13,14 +14,26 @@ export default class ForgotPasswordForm extends Component {
                 <form className="forgotPassword" onSubmit={this.props.onSubmit}> {/* 登录*/}
                     <div className="row">
                         <label>邮箱</label>
-                        <input type="text" value={this.props.formData.email}
+                        <input className="email" type="text" value={this.props.formData.email}
                                 onChange={this.props.onChange.bind(null, 'email')}/>
                     </div>
                     <div className="row actions">
-                        <button className="Button" type="submit">发送</button>
+                        <button onClick={this.isOk.bind(this)}className="Button" type="submit">发送</button>
                     </div>
                 </form>
             </div>
         )
+    }
+    isOk(){
+        var input=document.getElementsByClassName('email')
+        let regEmail = /\w+@/
+        if($(input).val() ===''){
+            alert('请填写邮箱再发送')
+        }else if($(input).val()!=='' && !regEmail.test()){
+            alert('邮箱格式不正确')
+        }
+        else {
+            alert('发送成功，请查看邮箱')
+        }
     }
 }
